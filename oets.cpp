0d1399c212f85ae3be48c00d97d91410227487f8
20dinara03 <xgarip00@stud.fit.vutbr.cz>
2025-04-02 18:34:20 +0200

    parallel reading binary data

--- oets.cpp
+++ oets.cpp
@@ -1,18 +1,27 @@
-#include <mpi.h>
 #include <iostream>
+#include <fstream>
+#include <mpi.h>
 
 int main(int argc, char** argv) {
-MPI_Init(&argc, &argv);
+    MPI_Init(&argc, &argv);
 
-int world_size;
-MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    int size;
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-int world_rank;
-MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-std::cout << "Hello from processor " << world_rank;
-std::cout << " of " << world_size << std::endl;
-
-MPI_Finalize();
-return 0;
+    std::ifstream infile("numbers", std::ios::binary);
+    if (!infile.is_open()) {
+        std::cerr << "Error: file didn't open!" << std::endl;
+        return 1;
+    }
+    
+    unsigned char byte;
+    infile.seekg(rank,std::ios_base::beg);
+    infile.read(reinterpret_cast<char*>(&byte), 1);
+    int number = static_cast<int>(byte);
+    std::cout << "Hello from processor " << rank;
+    std::cout << " my number is  " << number << std::endl;
+    return 0;
 }
